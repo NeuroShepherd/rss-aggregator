322d12b265e686907d10401033c5f7614d985e33
Patrick Callahan <patricktcallahan18@gmail.com>
2026-03-21 17:28:58 +0100

    Set-up main.go to import config module and run Read()

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/neuroshepherd/rss-aggregator/internal/config"
+)
+
 func main() {
-	print("Placeholder")
+	cfg, err := config.Read()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(cfg)
 }
