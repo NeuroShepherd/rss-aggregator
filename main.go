b1cd6a6e354018020e89c5d15d2886db721ec441
Patrick Callahan <patricktcallahan18@gmail.com>
2026-03-21 18:01:23 +0100

    Read, set, and read again the config file from main.go

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/neuroshepherd/rss-aggregator/internal/config"
 )
@@ -9,7 +10,21 @@ import (
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
-		panic(err)
+		fmt.Fprintln(os.Stderr, "read config:", err)
+		os.Exit(1)
+	}
+	fmt.Println(cfg)
+
+	err = cfg.SetUser("kenobi")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "set user:", err)
+		os.Exit(1)
+	}
+
+	cfg, err = config.Read()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "read config:", err)
+		os.Exit(1)
 	}
 	fmt.Println(cfg)
 }
