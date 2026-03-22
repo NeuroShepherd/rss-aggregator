767edaef5e7a2ad017b166de4d0ad03a42a985fc
Patrick Callahan <patricktcallahan18@gmail.com>
2026-03-22 11:27:34 +0100

    Create register handler function

---
+++ handler_user.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"time"
+
+	"os"
+
+	"github.com/google/uuid"
+	"github.com/neuroshepherd/rss-aggregator/internal/database"
+)
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("username is required")
+	}
+
+	name := cmd.args[0]
+
+	user, err := s.db.CreateUser(
+		context.Background(),
+		database.CreateUserParams{
+			ID:        uuid.New(),
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+			Name:      name,
+		},
+	)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "create user: %w", err)
+		os.Exit(1)
+	}
+
+	err = s.cfg.SetUser(name)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "set user: %w", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Registered user: %s (ID: %s)\n", user.Name, user.ID)
+	return nil
+}
