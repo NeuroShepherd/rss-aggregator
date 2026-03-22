e4d5fd1804cd29d4f07dece32bee3c540609df30
Patrick Callahan <patricktcallahan18@gmail.com>
2026-03-22 18:04:36 +0100

    Create handler for adding feeds to db
    Wire up to main.go

---
+++ handler_feed.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"time"
+
+	"github.com/google/uuid"
+
+	"github.com/neuroshepherd/rss-aggregator/internal/database"
+)
+
+func handlerAddFeed(s *state, cmd command) error {
+	if len(cmd.args) < 2 {
+		return fmt.Errorf("name and url must be provided")
+	}
+	name := cmd.args[0]
+	url := cmd.args[1]
+
+	if name == "" || url == "" {
+		return fmt.Errorf("name and url must be provided")
+	}
+
+	user, err := s.db.GetUser(context.Background(), s.cfg.CurrentUserName)
+	if err != nil {
+		return fmt.Errorf("get user: %w", err)
+	}
+
+	feed, err := s.db.CreateFeed(context.Background(), database.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      name,
+		Url:       url,
+		UserID:    user.ID,
+	})
+	if err != nil {
+		return fmt.Errorf("create feed: %w", err)
+	}
+
+	fmt.Printf("Feed created: %+v\n", feed)
+	return nil
+}
